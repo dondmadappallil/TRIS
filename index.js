6788fa2749a3fda20cda7a4ec504f6ae1c2797c8
dondmadappallil <dondmadappallil@gmail.com>
2018-05-28 15:02:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var request = require('request');
+
+exports.TRISV1 = (req, res) => {
+
+  let appliance = req.body.queryResult.parameters['Appliance'];
+  let trigger = req.body.queryResult.parameters[Trigger'];
+
+  var pin= '';
+  if ( appliance == 'Light')
+   pin='D1';
+  elseif ( appliance == 'Fan')
+   pin='D0';
+  elseif ( appliance == 'Plug')
+   pin='D2';
+
+  var state='';
+  if ( trigger == 'ON')
+   state=1;
+  else if ( trigger == 'OFF')
+   state=0;
+
+ request('http://188.166.206.43/108a9520b5f94806b4e5493ad4de816e/upda$
+  console.log('Status:', response.statusCode);
+  response.json({ fulfillmentText: 'Completed Switching'});
+  });
+
+  };
