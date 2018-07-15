ebc394e665c01045eaa036e5e94fe3fcc506e0de
dondmadappallil <dondmadappallil@gmail.com>
2018-07-15 10:07:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-
 var request = require('request');
 
 exports.TRISV1 = (req, res) => {
-
+  
+  res.json ();
+            
   let appliance = req.body.queryResult.parameters['Appliance'];
-  let trigger = req.body.queryResult.parameters[Trigger'] ;
+  let trigger = req.body.queryResult.parameters['Trigger'];
 
-  var pin= '';
+  var pin= '';  
   if ( appliance == 'Light')
    pin='D1';
-  elseif ( appliance == 'Fan')
+  else if ( appliance == 'Fan')
    pin='D0';
-  elseif ( appliance == 'Plug')
+  else if ( appliance == 'Plug')
    pin='D2';
 
   var state='';
@@ -20,9 +21,9 @@ exports.TRISV1 = (req, res) => {
   else if ( trigger == 'OFF')
    state=0;
 
- request('http://188.166.206.43/108a9520b5f94806b4e5493ad4de816e/update/D2?value=0', function (error, response, body) {
+ request('http://188.166.206.43/108a9520b5f94806b4e5493ad4de816e/update/'+pin+'?value='+state, function (error, response, body) {
   console.log('Status:', response.statusCode);
-  response.json({ fulfillmentText: 'Completed Switching'});
+  
   });
 
   };
